3c012c9e05f744d430ddd8acb8a7349551e5bc57
Denocle <denocle@gmail.com>
2013-04-13 00:24:07 +0200

    Revert "Updated configs and other stuff"
    This reverts commit e8ffcf3c4b8ab0b99c3aaf9e062b15f3effeca91.

--- installation/@Start.bat
+++ installation/@Start.bat
@@ -1,11 +1,12 @@
 @echo off
 echo.
 echo Starting server
-rem Resources\mysql.exe --user=%mysqluser% --password=%mysqlpass% --host=127.0.0.1 --port=3306 --database=%mysqldb% --execute="call pMain()"
+cd C:\DayZserver
 start .\Expansion\beta\arma2oaserver.exe -port=2302 -mod=@DayZ;@Server -name=Dayz -config=Config_DayZChernarus\ServerSettings.cfg -cfg=Config_DayZChernarus\arma2.cfg -profiles=Config_DayZChernarus
 echo.
 ping 127.0.0.1 -n 15 >NUL
 echo Starting BEC
 echo.
+cd C:\DayZserver\BEC
 start .\Bec.exe -f \config.cfg
 exit
